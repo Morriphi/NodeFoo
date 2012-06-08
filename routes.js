d1e998e4998d34ae4b87daad9147dc0a621d06be
Philip Morris <philiplmorris@googlemail.com>
2012-06-08 10:26:57 +0100

    added tests

--- routes.js
+++ routes.js
@@ -1,15 +1,19 @@
+var util = require('util');
+
 module.exports = {
   	register: function(app, callback)
 	{
 		app.get('/', function(request, response) {
 			response.render('index.jade', {
 				title: 'Express'
+				, message: util.format("Hello from %s and %s", "Node Js", "Express")
 			});
 		});
 		
 		app.get('/node', function(request, response) {
 			response.render('index.jade', {
 				title: 'Node'
+				, message: util.format("Hello from %s and %s", "Node Js", "Express")
 			});
 		});
 
